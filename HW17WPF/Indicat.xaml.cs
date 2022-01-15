a0cb490b752a01fe8cbc8cf2bc188f43a15d2134
Магди Османов <magdi.osmanov97@gmail.com>
2022-01-16 01:39:22 +0300

    Добавьте файлы проекта.

---
+++ HW17WPF/Indicat.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW17WPF
+{
+    /// <summary>
+    /// Логика взаимодействия для Indicat.xaml
+    /// </summary>
+    public partial class Indicat : UserControl
+    {
+        public static readonly DependencyProperty ValueProperty =
+        DependencyProperty.Register(
+            nameof(Value), 
+            typeof(double), 
+            typeof(Indicat),
+            new FrameworkPropertyMetadata(default(double),0
+            ,null
+            ,new CoerceValueCallback(CoerceMyValue)));
+
+        private static object CoerceMyValue(DependencyObject d, object baseValue)
+        {
+            double v = (double)baseValue;
+            if (v<0)
+            {
+                return 0;
+            }
+            if (v>180)
+            {
+                return 180;
+            }
+            return v;
+        }
+
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set =>SetValue(ValueProperty,value);
+        }
+        public Indicat()
+        {
+            InitializeComponent();
+        }
+    }
+}
